cf31c9b547ef0c74f05f9afadc5dea55f44fb06d
artiannan <artiannan@icloud.com>
2023-02-26 23:34:19 -0800

    finalchange1

--- module4-solution/SpeakGoodbye.js
+++ module4-solution/SpeakGoodbye.js
@@ -1,7 +1,7 @@
 (function(window) {
-    var byeSpeaker = new Object();
+    var byeSpeaker = {};
     var speakWord = "Good Bye";
-    byeSpeaker.speak = function speak(name) {
+    byeSpeaker.speak = function (name) {
         console.log(speakWord + " " + name);
     };
     window.byeSpeaker = byeSpeaker;
