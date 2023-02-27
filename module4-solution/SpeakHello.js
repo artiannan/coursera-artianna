491f51ccc603ffc70fd057bf7c1fb2feac4a7829
artiannan <artiannan@icloud.com>
2023-02-26 23:32:29 -0800

    Change1

--- module4-solution/SpeakHello.js
+++ module4-solution/SpeakHello.js
@@ -1,8 +1,9 @@
 (function(window) {
-    var helloSpeaker = new Object();
+    var helloSpeaker = {};
     var speakWord = "Hello";
     helloSpeaker.speak = function(name) {
         console.log(speakWord + " " + name);
-    };
+    }
+    
     window.helloSpeaker = helloSpeaker;
 })(window);
