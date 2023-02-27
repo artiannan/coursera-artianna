ac4d5e9d6b4067e78c2b542705762808ee298c0f
artiannan <artiannan@icloud.com>
2023-02-26 23:11:13 -0800

    creation5

--- module4-solution/script.js
+++ module4-solution/script.js
@@ -17,8 +17,10 @@ Good Bye Jim
 
 (function() {
     var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+    
     for (var i = 0; i < names.length; i++) {
         var firstLetter = names[i].charAt(0).toLowerCase();
+
         if (firstLetter === 'j') {
             byeSpeaker.speak(names[i]);
         } 
